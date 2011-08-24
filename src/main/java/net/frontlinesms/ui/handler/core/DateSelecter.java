7ae585dcde7f8c82071ec9a3c216596e1f8e7211
Dieterich Lawson <dieterich.lawson@gmail.com>
2011-08-24 18:21:38 -0500

    Updated DateSelecter to use the Ethiopic calendar.

--- src/main/java/net/frontlinesms/ui/handler/core/DateSelecter.java
+++ src/main/java/net/frontlinesms/ui/handler/core/DateSelecter.java
@@ -21,16 +21,15 @@ package net.frontlinesms.ui.handler.core;
 
 import java.awt.Color;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.Calendar;
-import java.util.Date;
 
-import org.apache.log4j.Logger;
-
-import net.frontlinesms.FrontlineSMSConstants;
 import net.frontlinesms.FrontlineUtils;
 import net.frontlinesms.ui.UiGeneratorController;
 import net.frontlinesms.ui.i18n.InternationalisationUtils;
+
+import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+
 import thinlet.Thinlet;
 
 /**
@@ -43,7 +42,7 @@ public class DateSelecter {
 	/** Index of the first month, January */
 	private static final int FIRST_MONTH = 0;
 	/** Index of the last month, December */
-	private static final int LAST_MONTH = 11;
+	private static final int LAST_MONTH = 12;
 	private static final String COMPONENT_LB_MONTH = "lbMonth";
 	private static final String COMPONENT_BT_NEXT = "btNext";
 	private static final String COMPONENT_BT_PREVIOUS = "btPrevious";
@@ -53,7 +52,7 @@ public class DateSelecter {
 	/** Logger for this class */
 	private final Logger log = FrontlineUtils.getLogger(getClass());
 	/** A calendar object with a poorly-defined role */
-	private Calendar current;
+	private DateTime current;
 	/** A month value with a poorly-defined role */
 	private int curMonth;
 	/** A year value with a poorly-defined role */
@@ -65,6 +64,13 @@ public class DateSelecter {
 	private int dayToHighlight;
 	private int monthToHighlight;
 	private int yearToHighlight;
+	
+	private final static String[] ethiopicMonths = new String[]{
+		"M\u00E4sk\u00E4r\u00E4m","\u1E6C\u0259q\u0259mt", "\u1E2A\u0259dar",
+		"Ta\u1E2B\u015Ba\u015B", "\u1E6C\u0259rr","Y\u00E4katit",
+		"M\u00E4gabit","Miyazya","G\u0259nbot","S\u00E4ne",
+		"\u1E24amle","N\u00E4hase","\u1E56ag\u02B7\u0259men"
+	};
 	
 //> CONSTRUCTORS
 	/**
@@ -100,20 +106,19 @@ public class DateSelecter {
 		ui.setAction(prev, "previousMonth(dateSelecter)", dialog, this);
 		Object next = ui.find(dialog, COMPONENT_BT_NEXT);
 		ui.setAction(next, "nextMonth(dateSelecter)", dialog, this);
-		current = Calendar.getInstance();
+		current = DateTime.now(InternationalisationUtils.ethiopicChronology);
 		setDayToHighlight();
-		current.set(Calendar.DATE, 1);
+		current = new DateTime(current.getYear(),current.getMonthOfYear(),1,0,0,InternationalisationUtils.ethiopicChronology);
 		if (!ui.getText(textField).equals("")) {
 			log.debug("Previous date is [" + ui.getText(textField) + "]");
 			try {
-				Date d = InternationalisationUtils.getDateFormat().parse(ui.getText(textField));
-				current.setTime(d);
+				current = InternationalisationUtils.getDateFormat().parseDateTime(ui.getText(textField));
 				setDayToHighlight();
-			} catch (ParseException e) {}
+			} catch (Exception e) {}
 		}
-		log.debug("Current date is [" + current.getTime() + "]");
-		curMonth = current.get(Calendar.MONTH);
-		curYear = current.get(Calendar.YEAR);
+		log.debug("Current date is [" + current.toString() + "]");
+		curMonth = current.getMonthOfYear();
+		curYear = current.getYear();
 		log.trace("EXIT");
 	}
 
@@ -131,12 +136,9 @@ public class DateSelecter {
 	 * @param dialog
 	 */
 	public void nextMonth(Object dialog) {
-		current.set(Calendar.MONTH, this.curMonth + 1);
-		curMonth = current.get(Calendar.MONTH);
-		if (curMonth == FIRST_MONTH) {
-			current.set(Calendar.YEAR, this.curYear + 1);
-		}
-		curYear = current.get(Calendar.YEAR);
+		current = current.plusMonths(1);
+		curMonth = current.getMonthOfYear();
+		curYear = current.getYear();
 		showMonth(dialog);
 	}
 	
@@ -146,12 +148,9 @@ public class DateSelecter {
 	 * @param dialog
 	 */
 	public void previousMonth(Object dialog) {
-		current.set(Calendar.MONTH, this.curMonth - 1);
-		curMonth = current.get(Calendar.MONTH);
-		if (curMonth == LAST_MONTH) {
-			current.set(Calendar.YEAR, this.curYear - 1);
-		}
-		curYear = current.get(Calendar.YEAR);
+		current = current.minusMonths(1);
+		curMonth = current.getMonthOfYear();
+		curYear = current.getYear();
 		showMonth(dialog);
 	}
 	
@@ -162,23 +161,21 @@ public class DateSelecter {
 	 */
 	private void showMonth(Object dialog) {
 		log.trace("ENTER");
-		current.set(Calendar.DATE, 1);
-		String curMonth = getMonthAsString(this.curMonth) + " " + curYear;
+		current = new DateTime(current.getYear(),current.getMonthOfYear(),1,2,2,InternationalisationUtils.ethiopicChronology);
+		String curMonth = ethiopicMonths[current.getMonthOfYear() -1] + " " + curYear;
 		Object lbMonth = ui.find(dialog, COMPONENT_LB_MONTH);
 		log.debug("Current month [" + curMonth + "]");
 		ui.setText(lbMonth, curMonth);
 		for (int i = 1; i <= 6; i++) {
 			fillRow(dialog, "pn" + i);
 		}
+		if(current.getMonthOfYear() != this.curMonth) current = current.minusMonths(1);
 		log.trace("EXIT");
 	}
 
 	public void selectionMade(Object dialog, String day) {
-		Calendar c = Calendar.getInstance();
-		c.set(Calendar.DATE, Integer.parseInt(day));
-		c.set(Calendar.MONTH, this.curMonth);
-		c.set(Calendar.YEAR, this.curYear);
-		String date = InternationalisationUtils.getDateFormat().format(c.getTime());
+		DateTime dt = new DateTime(this.curYear,this.curMonth,Integer.parseInt(day),0,0,InternationalisationUtils.ethiopicChronology);
+		String date = InternationalisationUtils.getDateFormat().print(dt);
 		ui.setText(textField, date);
 		ui.remove(dialog);
 		ui.invokeAction(this.textField);
@@ -187,9 +184,9 @@ public class DateSelecter {
 //> UI HELPER METHODS
 	/** Sets the day to be highlighted */
 	private void setDayToHighlight() {
-		dayToHighlight = current.get(Calendar.DATE);
-		monthToHighlight = current.get(Calendar.MONTH);
-		yearToHighlight = current.get(Calendar.YEAR);
+		dayToHighlight = current.getDayOfMonth();
+		monthToHighlight = current.getMonthOfYear();
+		yearToHighlight = current.getYear();
 	}
 	/**
 	 * Sets the button texts for the supplied week.
@@ -203,25 +200,23 @@ public class DateSelecter {
 		cleanButtons(buttons);
 		
 		int dayOfWeek;
-		while (current.get(Calendar.MONTH) == this.curMonth) {
-			dayOfWeek = current.get(Calendar.DAY_OF_WEEK);
-			Object button = buttons[dayOfWeek - 1];
+		while (current.getMonthOfYear() == this.curMonth) {
+			dayOfWeek = current.getDayOfWeek();
+			Object button = buttons[dayOfWeek-1];
 			ui.setEnabled(button, true);
-			ui.setText(button, String.valueOf(current.get(Calendar.DATE)));
+			ui.setText(button, String.valueOf(current.getDayOfMonth()));
 			ui.setAction(button, "selectionMade(dateSelecter, this.text)", dialog, this);
 			if (isDayToHighlight()) {
 				ui.setColor(button, Thinlet.FOREGROUND, Color.RED);
 			}
-			current.set(Calendar.DATE, current.get(Calendar.DATE) + 1);
+			current = current.plusDays(1);
 			if (dayOfWeek == Calendar.SATURDAY) {
 				//we've reached the end of the week, so we break;
 				break;
 			}
 		}
-		if (current.get(Calendar.YEAR) != curYear) {
-			current.set(Calendar.YEAR, curYear);
-		}
 	}
+	
 	private void cleanButtons(Object[] buttons) {
 		for (Object but : buttons) {
 			ui.setText(but, "");
@@ -230,16 +225,8 @@ public class DateSelecter {
 		}
 	}
 	private boolean isDayToHighlight() {
-		return (current.get(Calendar.DATE) == dayToHighlight 
-				&& current.get(Calendar.MONTH) == monthToHighlight
-				&& current.get(Calendar.YEAR) == yearToHighlight);
-	}
-	/**
-	 * Gets the name of the month with the specified index
-	 * @param i 0-based index of the month: 0=jan, 11=dec
-	 * @return Month name
-	 */
-	private String getMonthAsString(int i) {
-		return InternationalisationUtils.getI18nString(FrontlineSMSConstants.MONTH_KEYS[i]);
+		return (current.getDayOfMonth() == dayToHighlight 
+				&& current.getMonthOfYear() == monthToHighlight
+				&& current.getYear() == yearToHighlight);
 	}
 }
